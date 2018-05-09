6bda3fab207f209ea1a6bafbce8cf230b03ef0b1
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2018-05-09 12:24:11 +0200

    Moving forward

--- src/Posts/_data.js
+++ src/Posts/_data.js
@@ -1,4 +1,44 @@
 var data = {
+    'tuk-tuk': {
+        'title': 'La trampa del tuk-tuk en Bangkok',
+        'date': [15, 'Marzo', 2016],
+        'description': 'Antes de subirte a un tuk-tuk de Bangkok, deberías tener cuidado cuando te prometen viajes a 40 bahts. Ojo, es una trampa.',
+    },   
+    'perdido': {
+        'title': 'Perdido en Bangkok',
+        'date': [13, 'Marzo', 2016],
+        'description': 'Desorientado, desubicado, perdido... Así me he sentido en la primera vez que visito el Reino de Siam. Ah, por cierto, Bangkok huele a «caldofrán».',
+    },
+    'cuenta-atras': {
+        'title': 'Los días previos a marcharse lejos',
+        'date': [7, 'Marzo', 2016],
+        'description': 'Comienza la cuenta atrás. No queda nada para abandonar España. He aquí algunas de las cosillas que me pasan por la cabeza antes de partir.',
+    },
+    'nuevas-rutinas': {
+        'title': 'Nuevas rutinas',
+        'date': [2, 'Marzo', 2016],
+        'description': 'El día que me desenganché del gimnasio después de llevar diez años entrenando allí. Ya estoy dado de baja. Ha sido como dejar de cepillarme los dientes.',
+    },
+    'ansiedad-del-viajero': {
+        'title': 'Ansiedad del viajero',
+        'date': [29, 'Febrero', 2016],
+        'description': 'Hoy es el último día de este extraño mes de febrero. He vuelto a dormir mal, y es que empiezo a estar realmente nervioso por el viaje.',
+    },
+    'petar': {
+        'title': 'A muchos kilómetros',
+        'date': [26, 'Febrero', 2016],
+        'description': 'De cómo conocí a Petar y cómo nos hicimos amigos en Alemania, estando de Erasmus. A veces, las mejores cosas se encuentran a muchos kilómetros de casa.',
+    },
+    'digital': {
+        'title': 'Nosotros solamente pasábamos por ahí',
+        'date': [22, 'Febrero', 2016],
+        'description': 'Era digital: mis nietos preguntándome qué cojones era un bolígrafo y qué enfermedad teníamos para cortar árboles que nos permitiesen escribir en papel.',
+    },
+    'remando-a-favor': {
+        'title': 'Remando a favor',
+        'date': [18, 'Febrero', 2016],
+        'description': 'La publicidad nos engaña, persuade, seduce y manipula para que compremos mierda que no necesitamos.',
+    },
     'aceptar-condiciones-privacidad': {
         'title': 'Acepta las condiciones de privacidad',
         'date': [10, 'Febrero', 2016],
