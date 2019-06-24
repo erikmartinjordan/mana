2b01ef60f02ceec77b9fedbe208ccc229855ddbe
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2019-06-24 12:43:10 +0700

    Update night mode things

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -83,6 +83,7 @@ class Nav extends Component {
                       </React.Fragment>
                     : <div className = 'User'>
                             <div className = 'Img-Wrap'>
+                                
                                 <img onClick = {this.showMenu}  src = {this.state.user.photoURL}></img>        
                                 <Link to = '/' onClick = {this.showPost} className = 'New-Post'>Publicar</Link>
                             </div>
@@ -93,6 +94,8 @@ class Nav extends Component {
                                 <Link to = '/'>Comunidad</Link>
                                 <Link to = '/Blog'>Blog</Link> 
                                 <Link to = '/Acerca'>Acerca</Link>
+                                <div className = 'Separator'></div>
+                                <a onClick = {this.changeTheme}>Cambiar a modo {this.state.theme === 'dark' ? 'día' : 'noche'}</a>
                                 <div className = 'Separator'></div>
                                 <Link to = '/'><div onClick = {this.signOut} className = 'Logout'>Cerrar sesión</div></Link>
                               </div>
