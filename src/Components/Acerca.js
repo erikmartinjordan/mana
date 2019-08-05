deafcf57e1e94cfa6e7dfd2b188752842f736b63
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2019-08-05 14:01:31 +0700

    Creating Acerca page... (wip)

--- src/Components/Acerca.js
+++ src/Components/Acerca.js
@@ -1,78 +1,89 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase, { auth } from '../Functions/Firebase';
 import { Link } from 'react-router-dom';
 import '../Styles/Acerca.css';
 
-class Acerca extends Component {
+const Acerca = () => {
     
-  componentDidMount = () => {
-      //Meta and title
-      document.title = 'Acerca - Nomoresheet'; 
-      document.querySelector('meta[name="description"]').content = 'De cómo nació el sitio web y los diferentes hitos hasta la fecha.'; 
-      
-      window.twemoji.parse(document.getElementById('root'), {folder: 'svg', ext: '.svg'} );
-  }
-  
-  render() { 
-      
-    var data = [
-        {
-            'title': 'Mejoras sutiles',
-            'date': [26, 'Junio', 2019],
-            'description': 'Mejoras de la última versión:',
-            'list': {
-                'elem1': '💬 Añadidos últimos comentarios en la portada.',
-                'elem2': '📈 Añadidas estadísticas de usuarios en perfil.',
-                'elem3': '🌙 Creado modo noche.'
-            }
-        },
-        {
-            'title': 'Vuelvo a Tailandia',
-            'date': [14, 'Junio', 2019],
-            'description': 'Después de casi dos años, vuelvo a Tailandia otra vez. Ganas de crear cosas nuevas.',
-        },
-        {
-            'title': 'Tres años',
-            'date': [22, 'Diciembre', 2018],
-            'description': 'Nace la página «acerca de», donde se recopilarán algunos hitos hasta la fecha. Esta pestaña está en construcción e ira modificándose a medida que pasen los días.',
-        },
-        {
-            'title': 'Comunidad',
-            'date': [13, 'Diciembre', 2017],
-            'description': 'Se crea la comunidad. Un foro donde se habla del Sudeste Asiático, de Tailandia y de cualquier tema que pueda ser de interés para el resto de los mortales.',
-        },
-        {
-            'title': 'Primer artículo',
-            'date': [9, 'Diciembre', 2015],
-            'description': 'Primer artículo en Nomoresheet. Fue publicado en un blog cutrillo de WordPress sin dominio propio. El título de ese primer artículo ha cambiado en innumerables ocasiones.',
-        },
-    ]
+    const today = new Date();
+    const month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    const [admin, setAdmin] = useState(null);
+    const [content, setContent] = useState(null);
+    const [data, setData] = useState(null);
+    const [date, setDate] = useState(today.getDate() + ' de ' + month[today.getMonth()] + ' del ' + today.getFullYear());
+    const [title, setTitle] = useState('Título');
     
+    useEffect( () => {
+        //Meta and title
+        document.title = 'Acerca - Nomoresheet'; 
+        document.querySelector('meta[name="description"]').content = 'De cómo nació el sitio web y los diferentes hitos hasta la fecha.'; 
+        
+        // Checking if admin is connected
+        auth.onAuthStateChanged( user => { user && user.uid === 'dOjpU9i6kRRhCLfYb6sfSHhvdBx2' && setAdmin(true) });
+        
+        // Fetching data from Database
+        !data && firebase.database().ref('features/').on('value', snapshot => { if(snapshot.val()) setData(snapshot.val().reverse()) });
+        
+        // Emojis in svg
+        window.twemoji.parse(document.getElementById('root'), {folder: 'svg', ext: '.svg'} );
+    }, [data]);
+    
+    const upload = () => {
+        
+        
+    }
+
     return (
       <div className = 'Acerca'>
-        
+
         <h2>Acerca</h2>
-        
+
         <div className = 'Intro'>
             <img src = 'https://lh6.googleusercontent.com/-WwLYxZDTcu8/AAAAAAAAAAI/AAAAAAAAZF4/6lngnHRUX7c/photo.jpg'></img>
             <p>Hola 👋🏻<br></br> Soy Erik, el creador de Nomoresheet. En diciembre del 2015 nació esta web. Aquí encontrarás algunos de los hitos más relevantes hasta la fecha.</p>
         </div>
         
-        {data.map((item, key) =>
+        {admin &&
+            [<div className = 'Block'>
+                <input      onChange = {(e) => setTitle(e.target.value)} 
+                            className = 'Title' 
+                            placeholder = 'Título...' 
+                            value = {title}>
+                </input>
+                <div className = 'Date'>
+                    {date}
+                </div>
+                <textarea   onChange = {(e) => setContent(e.target.value)}
+                            className = 'Content'
+                            placeholder = 'Contenido...'
+                            value = {content}>
+                </textarea>
+                <div className = 'Buttons'>
+                    <button className = 'Picture'>📸</button>
+                    <button className = 'send'>Añadir</button>
+                </div>
+            </div>,
+            <div className = 'Separator'></div>]           
+        }
+
+        {data && data.map((item, key) =>
             [<div className = 'Block'>
                 {item.title ? <h3>{item.title}</h3> : null}
-                <div className = 'Date'><p>{item.date[0] + ' de ' + item.date[1] + ' del '  + item.date[2]}</p></div>
-                    <div className = 'Content'>
-                        <div className = 'Text'>
-                            {item.description ? <p>{item.description}</p> : null}
-                            {item.list ? Object.keys(item.list).map((value, key) => <li>{item.list[value]}</li>) : null}
-                        </div>
+                <div className = 'Date'>{item.date[0] + ' de ' + item.date[1] + ' del '  + item.date[2]}</div>
+                <div className = 'Content'>
+                    <div className = 'Text'>
+                        {item.description && <p>{item.description}</p>}
+                        {item.list && Object.keys(item.list).map((value, key) => <li>{item.list[value]}</li>)}
+                        {item.pic  && <img src = {item.pic}></img>}
                     </div>
+                </div>
+                {admin && <button className = 'Delete'>Eliminar</button>}
             </div>,
             <div className = 'Separator'></div>])
         }
       </div>
     );
-  }
+
 }
 
 export default Acerca;
