f4b986e3468f5c9a8fb90d43cb401bc6c955322d
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2019-10-30 23:51:48 +0800

    Modified component to show banner after beginning to read privat article

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,7 +1,7 @@
 import React, { Component, useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 import Fingerprint from 'fingerprintjs';
-import firebase, { auth } from '../Functions/Firebase';
+import firebase, { auth, provider } from '../Functions/Firebase';
 import Default from './Default';
 import Login from './Login';
 import Data from '../Posts/_data';
@@ -13,11 +13,12 @@ const Post = (props) => {
     const [description, setDescription] = useState(null);
     const [error, setError] = useState(false);
     const [likes, setLikes] = useState('');
+    const [privatPosts, setPrivatPosts] = useState(null);
     const [render, setRender] = useState(false);
     const [superlikes, setSuperlikes] = useState('');
     const [text, setText] = useState(null);
     const [title, setTitle] = useState('');
-    const [user, setUser] = useState(true);
+    const [user, setUser] = useState(false);
     const [views, setViews] = useState(null);
     const url = props.match.params.string;
     
@@ -31,27 +32,32 @@ const Post = (props) => {
 
     useEffect( () => { 
         
-      auth.onAuthStateChanged( user => setUser(user ? user : null) );
+        // Check if user is logged in
+        auth.onAuthStateChanged( user => setUser(user ? user : null) );
         
-      try{
+        // Count number of privat posts
+        let counter = 0;
+        Object.keys(Data).map( key => Data[key].privat && setPrivatPosts(counter => counter + 1) );
+        
+        try{
           // Get .md post
           const readmePath = require('../Posts/' + url + '.md'); 
-          
+
           // Fecth response and load Instagram and Twitter scripts
           fetch(readmePath).then(response => response.text()).then(text => setText(text)).then( () => {
               if(window.instgrm) window.instgrm.Embeds.process();
               if(window.twttr) window.twttr.widgets.load();
           });
-          
+
           // Get info from Json
           let title         = Data[url].title;
           let date          = Data[url].date;
           let description   = Data[url].description;
-          
+
           setTitle(title);
           setDate(date);
           setDescription(description);
-          
+
           // Get infro from Views and Likes
           firebase.database().ref('articles/' + url).on('value', snapshot => {
                 if(snapshot.val()){
@@ -60,15 +66,15 @@ const Post = (props) => {
                     setSuperlikes(snapshot.val().superlikes)
                 }
           })
-          
+
           // Update counters 
           firebase.database().ref('articles/' + url + '/views/').transaction( value => value + 1 );
-          
-      }
-      catch(e){
+
+        }
+        catch(e){
           // Md content doesn't exist
           setError(true);
-      }  
+        }  
         
     }, [])
     
@@ -106,6 +112,8 @@ const Post = (props) => {
 
         return res;
     }
+    
+    const loginUser = () => { auth.signInWithPopup(provider).then( result => setUser(result.user) ); }
 
     return (
     <div className = 'Post'>
@@ -124,55 +132,72 @@ const Post = (props) => {
                     </div>
                 </div>,
                 <div className = 'Content'>
-                    {text &&
-                    <ReactMarkdown 
-                    source = {text} 
-                    escapeHtml = {false} 
-                    renderers = {{link : props => <a href = {props.href} target = '_blank' rel = 'noindex noreferrer noopener'>{props.children}</a>}}
-                    /> 
-                    }
-                </div>,
-                <div className = 'Infopost'>
-                    <div></div>
-                    <div className = 'i'>👀 {parseInt(views).toLocaleString('es')}</div>
-                    <div className = 'i' onClick = {user ? handleLikes : () => setRender(true)}>👏 {likes}</div>
-                    <div className = 'i' onClick = {user ? handleSuperLikes : () => setRender(true)}>🎉 {superlikes}</div>
-                </div>,
-                <div className = 'Related'>
-                    <h2>Más cosas...</h2>
-                    <div className = 'Three-Block'>
-                        <a href = {'/'} className = 'Community'>
-                            <p>🐝 Accede a la comunidad: ¡Pregunta, opina y comenta!</p>
-                            <div className = 'Community-Grid'>
-                                <div>👵🏿</div>
-                                <div>👱🏾‍♀️</div>
-                                <div>👵🏽</div>
-                                <div>👳🏼</div>
-                                <div>👨🏻‍💻 </div>
-                            </div>
-                            <span className = 'Tag Green'>Comunidad →</span>
-                        </a>
-                        {views > 1000 &&
-                        <a onClick = {handleAd} 
-                            target = '_blank'
-                            href = 'https://www.agoda.com/partners/partnersearch.aspx?pcs=1&cid=1772106&hl=es&city=9395' 
-                            className = 'Ad'>
-                            <p>✨ Reserva tu hotel en Agoda. Precios mejores que en Booking.</p>
-                            <span className = 'Tag Yellow'>Ver hoteles →</span>
-                        </a>
-                        }
-                        <a onClick = {() => setRender(true)} className = 'Otro'>
-                            <p>👋 Accede a Nomoresheet para votar y comentar.</p>
-                            <span className = 'Access'>Acceder →</span>
-                        </a>
-                        {relatedContent()}
+                    { Data[url].privat && !user
+                    ? text && 
+                    <React.Fragment>
+                        <div className = 'Blur-Login'>
+                            <ReactMarkdown 
+                            source = {text.split('\n')[0] + '\n' + text.split('\n')[1] + '\n' + text.split('\n')[2] + '\n' + text.split('\n')[3]} 
+                            escapeHtml = {false} 
+                            renderers = {{link : props => <a href = {props.href} target = '_blank' rel = 'noindex noreferrer noopener'>{props.children}</a>}}
+                            /> 
+                        </div>
+                        <div className = 'Login-Box'>
+                            <h3>Lee la historia completa</h3>
+                            <p>Para poder seguir leyendo este artículo y {privatPosts} más, accede a la comunidad.</p>
+                            <a className = 'login' onClick = {() => setRender(true)}>Acceder</a>
+                        </div>
+                    </React.Fragment>
+                    : text &&
+                    <React.Fragment>
+                        <ReactMarkdown 
+                        source = {text} 
+                        escapeHtml = {false} 
+                        renderers = {{link : props => <a href = {props.href} target = '_blank' rel = 'noindex noreferrer noopener'>{props.children}</a>}}
+                        /> 
+                        <div className = 'Infopost'>
+                            <div></div>
+                            <div className = 'i'>👀 {parseInt(views).toLocaleString('es')}</div>
+                            <div className = 'i' onClick = {user ? handleLikes : () => setRender(true)}>👏 {likes}</div>
+                            <div className = 'i' onClick = {user ? handleSuperLikes : () => setRender(true)}>🎉 {superlikes}</div>
+                        </div>
+                        <div className = 'Related'>
+                        <h2>Más cosas...</h2>
+                        <div className = 'Three-Block'>
+                            <a href = {'/'} className = 'Community'>
+                                <p>🐝 Accede a la comunidad: ¡Pregunta, opina y comenta!</p>
+                                <div className = 'Community-Grid'>
+                                    <div>👵🏿</div>
+                                    <div>👱🏾‍♀️</div>
+                                    <div>👵🏽</div>
+                                    <div>👳🏼</div>
+                                    <div>👨🏻‍💻 </div>
+                                </div>
+                                <span className = 'Tag Green'>Comunidad →</span>
+                            </a>
+                            {views > 1000 &&
+                            <a onClick = {handleAd} 
+                                target = '_blank'
+                                href = 'https://www.agoda.com/partners/partnersearch.aspx?pcs=1&cid=1772106&hl=es&city=9395' 
+                                className = 'Ad'>
+                                <p>✨ Reserva tu hotel en Agoda. Precios mejores que en Booking.</p>
+                                <span className = 'Tag Yellow'>Ver hoteles →</span>
+                            </a>
+                            }
+                            <a onClick = {() => setRender(true)} className = 'Otro'>
+                                <p>👋 Accede a Nomoresheet para votar y comentar.</p>
+                                <span className = 'Access'>Acceder →</span>
+                            </a>
+                            {relatedContent()}
+                        </div>
                     </div>
+                    </React.Fragment>
+                    }
                 </div>]
                 :
                 <Default></Default>
             }
             {render && <Login hide = {() => setRender(false)}/>}
-            {Data[url].privat && !user && <Login hide = {() => setRender(false)}/>}
     </div>
     );
 }
