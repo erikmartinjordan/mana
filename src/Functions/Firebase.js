5e9502851d1c89b4083d56795224f051e80c988d
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2019-11-01 00:25:43 +0800

    Added two configs

--- src/Functions/Firebase.js
+++ src/Functions/Firebase.js
@@ -1,6 +1,16 @@
 import firebase from 'firebase'
 
-  var config = {
+var configPRE = {
+    apiKey: "AIzaSyCUBpYspYtPSoNKjFx86Y5xHJkXp3dPcO0",
+    authDomain: "nomoresheet-pre.firebaseapp.com",
+    databaseURL: "https://nomoresheet-pre.firebaseio.com",
+    projectId: "nomoresheet-pre",
+    storageBucket: "nomoresheet-pre.appspot.com",
+    messagingSenderId: "809572059318",
+    appId: "1:809572059318:web:dcbab69066a1dddc"
+}
+
+  var configPRO = {
     apiKey: "AIzaSyCI6dpu54CeFd1NOH5s7B-sHeK3KdEH5KU",
     authDomain: "nomoresheet-forum.firebaseapp.com",
     databaseURL: "https://nomoresheet-forum.firebaseio.com",
@@ -9,7 +19,7 @@ import firebase from 'firebase'
     messagingSenderId: "878815391785"
   };
 
-firebase.initializeApp(config);
+firebase.initializeApp(configPRO);
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
