d6957a33dfb31d725b8caec683b8e289af578039
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2019-11-07 12:06:21 +0800

    Adapted Users.js to hooks

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import firebase, { auth }   from '../Functions/Firebase';
-import Chart from 'chart.js';
+import React, { useState, useEffect }   from 'react';
+import Chart                            from 'chart.js';
+import firebase, { auth }               from '../Functions/Firebase';
 
-class Users extends Component {
+const Users = () => {
 
-  state = {
+    let object = {
       type: 'line',
       data: {
         labels: [],
@@ -51,58 +51,47 @@ class Users extends Component {
         },
         responsive: true
       }
-  }
+    }
     
-  componentDidMount = () => {
+    useEffect( () => {
 
-    let object = this.state;  
-    let canvas = document.querySelector('canvas');
-    let ctx = canvas.getContext('2d');
-    let width = window.innerWidth;
-    let gradientStroke = ctx.createLinearGradient(0, 0, width, 0);
-      
-    // Getting the number of users
-    firebase.database().ref('stats/').on('value', snapshot => {
+        let canvas = document.querySelector('canvas');
+        let ctx = canvas.getContext('2d');
+        let width = window.innerWidth;
+        let gradientStroke = ctx.createLinearGradient(0, 0, width, 0);
         
-        if(snapshot.val()) {
-            // Declaring data
-            let json = snapshot.val();
-            
-            // Getting labels and data
-            let labels = Object.keys(json).map( date => date.slice(6, 8) + '-' + date.slice(4, 6) + '-' + date.slice(0, 4));
-            let data   = Object.keys(json).map( date => Object.keys(json[date]).length);
-            
-            // Modifiying labels and data
-            object.data.labels = labels.slice(-7);
-            object.data.datasets['0'].data = data.reverse().slice(-7);
-                                    
-            // Updating state  
-            this.setState({ object });  
-        }
+        // Generating gradient color  
+        gradientStroke.addColorStop(0, '#00d2ff');
+        gradientStroke.addColorStop(0.5, '#3a7bd5');
         
-    }); 
-    
+        // Setting gradient stroke  
+        object.data.datasets['0'].borderColor = gradientStroke;  
+        
+        // Getting the number of users
+        firebase.database().ref('stats/').on('value', snapshot => {
+            
+            if(snapshot.val()) {
+                // Declaring data
+                let json = snapshot.val();
+                
+                // Getting labels and data
+                let labels = Object.keys(json).map( date => date.slice(6, 8) + '-' + date.slice(4, 6) + '-' + date.slice(0, 4));
+                let data   = Object.keys(json).map( date => Object.keys(json[date]).length);
+                
+                // Modifiying labels and data
+                object.data.labels = labels.slice(-7);
+                object.data.datasets['0'].data = data.reverse().slice(-7);
+            
+                // Drawing chart  
+                new Chart(ctx, object);  
+                
+            }
+            
+        }); 
+        
+    }, []);
 
-    // Generating gradient color  
-    gradientStroke.addColorStop(0, '#00d2ff');
-    gradientStroke.addColorStop(0.5, '#3a7bd5');
-
-    // Setting gradient stroke  
-    object.data.datasets['0'].borderColor = gradientStroke;      
-
-    // Drawing chart  
-    new Chart(ctx, this.state);      
-  }
-
-  componentDidUpdate = () => {
-    let canvas = document.querySelector('canvas');
-    let ctx = canvas.getContext('2d');
-    new Chart(ctx, this.state);       
-  }
-
-  render() {
     return <canvas/>
-  }
 }
 
 export default Users
