c9024ca9db1498b4d139845959c7d412330af3bc
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-01-07 13:00:21 +0700

    Force function to always return an array

--- src/Functions/GetPoints.js
+++ src/Functions/GetPoints.js
@@ -5,33 +5,24 @@ import GetNumberOfSpicy     from './GetNumberOfSpicy.js';
 import GetNumberOfApplauses from './GetNumberOfApplauses.js';
 import Points               from './PointsAndValues.js';
 
-//--------------------------------------------------------------/
-// This function returns the points of the user
-//
-//--------------------------------------------------------------/
 const GetPoints = (...userUids) => {
-        
-    // Defining the value of each element
+   
     const postValue     = Points.post;
     const replyValue    = Points.reply;
     const spicyValue    = Points.spicy;
     const applauseValue = Points.applause;
     
-    // Getting posts, replies, spicy and applauses
     const posts     = GetNumberOfPosts(...userUids);
     const replies   = GetNumberOfReplies(...userUids);
     const spicy     = GetNumberOfSpicy(...userUids);
     const applauses = GetNumberOfApplauses(...userUids);
     
-    // Initialize array
     const points = new Array(userUids.length).fill(0);
     
-    // Getting the points
     for(let i = 0; i < userUids.length; i ++)
         points[i] = (posts[i] * postValue) + (replies[i] * replyValue) + (spicy[i] * spicyValue) + (applauses[i] * applauseValue);
     
-    // Returning total points and the values of each element
-    return points.some(isNaN) ? 0 : points;
+    return points.some(isNaN) ? points.fill(0) : points;
 }
 
 export default GetPoints;
