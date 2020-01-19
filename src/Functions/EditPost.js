544de4ac327ee2744b4dbb17604abc143076843b
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-01-19 23:05:00 +0800

    Fixed edit post auto height

--- src/Functions/EditPost.js
+++ src/Functions/EditPost.js
@@ -1,78 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import firebase, {auth}               from './Firebase';
-import Alert                          from './Alert';
-import EmojiTextarea                  from './EmojiTextarea';
+import React, { useState, useEffect, useRef } from 'react';
+import firebase, {auth}                       from './Firebase';
+import Alert                                  from './Alert';
+import EmojiTextarea                          from './EmojiTextarea';
 import '../Styles/EditPost.css';
 
-//--------------------------------------------------------------/
-//
-//
-// This functions allows users to edit posts and reples
-//
-//
-//--------------------------------------------------------------/
 const EditPost = (props) => {
     
-    const [alert, setAlert] = useState(null);
+    const refTextarea           = useRef(null);
+    const [alert, setAlert]     = useState(null);
     const [message, setMessage] = useState(null);
+    
+    
+    useEffect( () => {
+        
+        if(refTextarea.current)
+            refTextarea.current.style.height = `${refTextarea.current.scrollHeight}px`;
+        
+        console.log(refTextarea);
+        
+    }, [message]);
         
     const editMessage = async () => {
         
-        // Getting the datapoint
         let reference;
         
         props.type === 'post'
         ? reference = firebase.database().ref('posts/' + props.post + '/message')
         : reference = firebase.database().ref('posts/' + props.post + '/replies/' + props.reply + '/message');
         
-        // Reading the message from database
         const snapshot = await reference.once('value');
         const message  = snapshot.val();
         
-        //Setting message 
         setMessage(message);
         
     }
     
     const handleMessage = (e) => {
         
-        // Resizing textarea after key press 
         e.target.style.height = 'inherit';
         e.target.style.height = `${e.target.scrollHeight}px`; 
         
-        // Setting new state using new message
         setMessage(e.target.value);
         
     }
     
     const submitMessage = () => {
         
-        // Getting the datapoint
         let reference;
         
         props.type === 'post'
         ? reference = firebase.database().ref('posts/' + props.post + '/message')
         : reference = firebase.database().ref('posts/' + props.post + '/replies/' + props.reply + '/message');
         
-        // Setting message
         reference.set(message);
         
-        // Getting edition time datapoint
         props.type === 'post'
         ? reference = firebase.database().ref('posts/' + props.post + '/edited')
         : reference = firebase.database().ref('posts/' + props.post + '/replies/' + props.reply + '/edited');
 
         
-        // Settting edition time
         reference.transaction(value => Date.now());
         
-        // Displaying alert
         setAlert(true);
         
-        // Setting timeOut to alert
         setTimeout( () => setAlert(false), 1500);
         
-        // Settin timeOut to message
         setTimeout( () => setMessage(null), 1500);
     }
 
@@ -81,7 +73,10 @@ const EditPost = (props) => {
             { message 
             ? <div className = 'Edit-Message'>
                 <div className = 'Edit-Message-Wrap'>
-                    <textarea value = {message} onChange = {(e) => handleMessage(e)}></textarea>
+                    <textarea ref      = {refTextarea} 
+                              value    = {message} 
+                              onChange = {(e) => handleMessage(e)} 
+                    />
                     <button onClick = {() => submitMessage()} className = 'bottom'>Guardar</button>
                 </div>
                 {message && <div className = 'Invisible' onClick = {() => setMessage(null)} ></div>}
