bed29e51a4d94fc2594a1c9e4bcf699297b2792f
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-03-12 12:41:03 +0800

    Refactored Notifications

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -8,117 +8,119 @@ import '../Styles/Notifications.css';
 
 const formatter = buildFormatter(spanishStrings);
 
-const Notifications = (props) => {  
+const Notifications = ({hide, user}) => {  
     
-    const [keys, setKeys] = useState([]);
+    const [keys, setKeys]                   = useState([]);
     const [notifications, setNotifications] = useState('loading');
-    const [points, setPoints] = useState(0);
-    const [show, setShow] = useState(false);
+    const [points, setPoints]               = useState(0);
+    const [show, setShow]                   = useState(false);
     
     useEffect( () => {
         window.twemoji.parse(document.getElementById('root'), {folder: 'svg', ext: '.svg'} );  
     });
 
     useEffect( () => {
-        
-        let mounted = true;
       
-        firebase.database().ref('notifications/' + props.user.uid).on('value', snapshot => { 
+        firebase.database().ref(`notifications/${user.uid}`).on('value', snapshot => { 
             
-            var notifications = snapshot.val();
-            var keys = notifications ? Object.keys(notifications) : [];
-            var array = [];
-
-            if(notifications && mounted){ 
-                notifications = keys.map( id => {
-                    return [notifications[id].points, notifications[id].message, notifications[id].read, notifications[id].timeStamp] 
-                });
-                
-                setKeys(keys);
-                setNotifications(notifications);
-                
-                keys.map( key => {
-                  firebase.database().ref('notifications/' + props.user.uid + `/${key}/read/`).transaction( read => read = true )
-                });
-            }
+            if(snapshot.val()) 
+                setNotifications(snapshot.val())
+            else               
+                setNotifications('empty');
             
-        });
-        
-        return () => { mounted = false };
-      
+        });  
       
     }, []);
-
-    const printNotifications = () => {
-        
-        let reverse = [...notifications].reverse();
-
-        let res = reverse.map( (notification, index) => {
-            
-                let printDate = true;
-                let thisNotificationTitle = last30DaysOrOlder(Date.now(), [notification[3]]);
-                
-                if(index > 0){
-                    
-                    let prevNotificationTitle = last30DaysOrOlder(Date.now(), reverse[index - 1][[3]]);
-                    let thisNotificationTitle = last30DaysOrOlder(Date.now(), reverse[index][[3]]);
-                    
-                    printDate = prevNotificationTitle !== thisNotificationTitle;
-                    
-                }
-
-                return <div key = {index} className = 'Notification'>
-                            { printDate
-                            ? <div  className = 'Notifications-Ago'>{thisNotificationTitle}</div>
-                            : null
-                            }
-                            <div className = 'Notifications-Content'>
-                                <span className = 'Notifications-Photo'>
-                                    <img src = {props.user.photoURL}></img>
-                                </span>
-                                <span className = 'Notifications-Points'>
-                                    { notification[0] > 0 
-                                    ? <span className = 'Pos'>+{notification[0]}</span>
-                                    : <span className = 'Neg'> {notification[0]}</span>
-                                    }
-                                </span>
-                                <span className = 'Notifications-Message'>
-                                    { notification[1] }
-                                    <TimeAgo formatter = {formatter} date = {notification[3]}/>
-                                </span>
-                            </div>
-                        </div>
-        });
-        
-        if(notifications === 'loading'){
-            
-            res = <div className = 'Notifications-Content-Empty'>
-                    <div className = 'Big-Emoji'>😼</div>
-                    <div className = 'Empty-Message'>Cargando notificaciones...</div>
-                </div>;
-        }
-           
-        if(notifications !== 'loading' && notifications.length === 0){
-            
-            res = <div className = 'Notifications-Content-Empty'>
-                    <div className = 'Big-Emoji'>😼</div>
-                    <div className = 'Empty-Message'>¡Miaaaaau! Aún no tienes notificaciones.</div>
-                </div>;
-        }
-        
-        return res;
-        
-    }
-
     
     return (
         <div className = 'Notifications'>
             <div className = 'Notifications-Wrap'>
-                <div className = 'Notifications-Menu'>{printNotifications()}</div>
+                <div className = 'Notifications-Menu'>
+                    { notifications === 'loading'
+                    ? <LoadingNotifications/>
+                    : notifications === 'empty'
+                    ? <EmptyNotifications/>
+                    : <ListNotifications notifications = {notifications} user = {user}/>
+                    }
+                </div>
             </div>
-            <div onClick = {props.hide} className = 'Invisible'></div>
+            <div onClick = {hide} className = 'Invisible'></div>
         </div>
     );
 }
 
 export default Notifications;
+
+
+const ListNotifications = ({notifications, user}) => {
+    
+    const [notificationsList, setNotificationsList] = useState([]);
+    
+    useEffect( () => {
+       
+        let sortedNotificationsArray = Object.values(notifications).sort( (a,b) => a.timeStamp > b.timeStamp ? -1 : 1);
+        
+        setNotificationsList(sortedNotificationsArray);
+        
+    }, []);
+    
+    const notificationTitle = (index) => {
+        
+        let thisTitle = last30DaysOrOlder(Date.now(), notificationsList[index].timeStamp);
+        let prevTitle = index > 0 ? last30DaysOrOlder(Date.now(), notificationsList[index - 1].timeStamp) : null;
+        
+        return thisTitle !== prevTitle ? thisTitle : null;
+        
+    }
+    
+    return (
+        <React.Fragment>
+            {notificationsList.map( ({points, message, read, date}, index) => 
+                <div key = {index} className = 'Notification'>
+                    {notificationTitle(index) 
+                    ? <div className = 'Notifications-Ago'>{notificationTitle(index)}</div> 
+                    : null
+                    }
+                    <div className = 'Notifications-Content'>
+                        <span className = 'Notifications-Photo'>
+                            <img src = {user.photoURL}></img>
+                        </span>
+                        <span className = 'Notifications-Points'>
+                            {points > 0 
+                            ? <span className = 'Pos'>+{points}</span>
+                            : <span className = 'Neg'> {points}</span>
+                            }
+                        </span>
+                        <span className = 'Notifications-Message'>
+                            {message}
+                            <TimeAgo formatter = {formatter} date = {date}/>
+                        </span>
+                    </div>
+                </div>
+            )}
+        </React.Fragment>
+    );
+    
+}
+
+const LoadingNotifications = () => {
+    
+    return(
+        <div className = 'Notifications-Content-Empty'>
+            <div className = 'Big-Emoji'>😼</div>
+            <div className = 'Empty-Message'>Cargando notificaciones...</div>
+        </div>
+    );
+    
+}
+
+const EmptyNotifications = () => {
+    
+    return (
+        <div className = 'Notifications-Content-Empty'>
+            <div className = 'Big-Emoji'>😼</div>
+            <div className = 'Empty-Message'>¡Miaaaaau! Aún no tienes notificaciones.</div>
+        </div>
+    );
+    
+}
