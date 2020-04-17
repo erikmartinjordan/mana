577e8130cd1772974eb67e3f5aefa49877e07194
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-04-17 10:00:18 +0200

    New function to count total views

--- src/Functions/GetNumberOfViews.js
+++ src/Functions/GetNumberOfViews.js
@@ -1,44 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import firebase, {auth} from './Firebase.js';
 
-//--------------------------------------------------------------/
-//
-//
-// This functions returns the name of a user 
-// with a specific userUid
-//
-//
-//--------------------------------------------------------------/
-const GetNumberOfViews = (userUid) => {
+const GetNumberOfViews = (uid) => {
     
-    const [views, setViews]   = useState(0);
-
-    // Getting users posts
+    const [views, setViews] = useState(0);
+    
     useEffect( () => { 
-        firebase.database().ref('posts/').on('value', snapshot => { 
-
-                    // Capturing data
-                    var posts = snapshot.val(); 
-                    var count = 0;
-
-                    // We sum spicy points of all the posts written by user with ID = uid
-                    if(posts){
-
-                        Object.keys(posts).map( id => { 
-
-                            // Counting the posts is easy, only increment value by one
-                            if(posts[id].userUid === userUid && posts[id].views) 
-                                count = count + posts[id].views;
-                            
-                        });
+        
+        if(uid){
+            
+            firebase.database().ref('posts/').on('value', snapshot => { 
+               
+                let posts = snapshot.val(); 
+                
+                if(posts){
+                    
+                    let userPosts = Object.values(posts).filter(post => {
                         
-                        setViews(count);
-                    }
-        });
+                        let { replies = {}, userUid } = post;
+                        
+                        return post.userUid === uid || Object.values(replies).some(reply => reply.userUid === uid);
+                        
+                    });
+                    
+                    let totalViews = userPosts.reduce( (total, post) => total = total + post.views, 0); 
+                    
+                    setViews(totalViews);
+                }
+               
+            });
+            
+        }
         
-    }, [userUid]);
-        
+    }, [uid]);
+    
     return views;
+    
 }
 
 export default GetNumberOfViews;
