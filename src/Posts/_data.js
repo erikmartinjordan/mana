6c6a6274414d90de8e2a7e555699246b3ebf6c2c
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-06-23 10:39:06 +0200

    New article

--- src/Posts/_data.js
+++ src/Posts/_data.js
@@ -1,4 +1,9 @@
 var data = {
+    'olvidar-a-alguien': {
+        'title': 'Cómo olvidar a alguien',
+        'date': [23, 'Junio', 2020],
+        'description': 'No puedes olvidar a alguien voluntariamente. Cuanto más quieres olvidar a una persona, más piensas en ella, más aparece en tu cabeza y más tiempo permanece ahí dando vueltas.',
+    },
     'estranged': {
         'title': '«Estranged»: ¿es siempre la familia lo primero?',
         'date': [21, 'Junio', 2020],
