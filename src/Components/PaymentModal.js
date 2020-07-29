10d3f779577627d71129f772a5933d55de1ba221
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-07-29 10:41:29 +0200

    Upgraded modal to work with PRE and PRO

--- src/Components/PaymentModal.js
+++ src/Components/PaymentModal.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import {Elements, StripeProvider} from 'react-stripe-elements';
-import CheckoutForm from '../Functions/CheckoutForm.js';
+import React, { useState, useEffect }  from 'react';
+import { Elements, StripeProvider }    from 'react-stripe-elements';
+import CheckoutForm                    from '../Functions/CheckoutForm';
+import { apiKey, environment }         from '../Functions/Firebase';
 import '../Styles/PaymentModal.css';
 
 const PaymentModal = (props) => {
-
-    return  <div className = 'Modal'>
-                <StripeProvider apiKey = 'pk_live_Hh2RhmFjzbX3kO24alpCEQLq003bNwQwf8'>
-                      <Elements>
-                        <CheckoutForm percentage = {props.percentage} hide = {props.hide} price = '19'/>
-                      </Elements>
-                </StripeProvider>
-            </div>
+    
+    return  (
+        <div className = 'Modal'>
+            <StripeProvider apiKey = {apiKey}>
+                <Elements>
+                    <CheckoutForm 
+                            account     = {props.account} 
+                            hide        = {props.hide} 
+                            plan        = {props.plan}
+                            environment = {environment}
+                    />
+                </Elements>
+            </StripeProvider>
+        </div>
+    );
+    
 }
 
 export default PaymentModal;
