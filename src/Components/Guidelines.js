a4c791284b94a39ce8900bf9efec4506ee6090c5
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-07-30 10:57:09 +0200

    Changed guidelines because the site is evolving

--- src/Components/Guidelines.js
+++ src/Components/Guidelines.js
@@ -10,7 +10,7 @@ const Guidelines = () => {
             <p>Por favor, échale un vistazo a las guías de publicación. Si las seguimos, la comunidad crecerá de forma sana.</p>
             <p>Las guías de publicación pueden ir mutando con el tiempo o verse modificadas por sugerencias o comportamientos de los usuarios.</p>
             <ol>
-                <li>Puedes escribir sobre lo que te apetezca: desde Tailandia hasta cualquier otro tema que pueda resultar de interés para la comunidad.</li>
+                <li>Puedes escribir sobre lo que te apetezca: cualquier tema que pueda resultar de interés para la comunidad es bienvenido.</li>
                 <li>Escribe como si estuvieses tomándote un café con alguien: sé amable, supón buena fe por parte de los demás, no seas puntilloso. </li>
                 <li>Cita la fuente original de tu publicación poniendo la URL sin recortar al final del mensaje.</li>
                 <li>Cuida tu ortografía.</li>
