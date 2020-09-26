fec3f4d0ab5d406fd6a96d3c0e7e9bdbca6b21d4
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-09-26 12:11:35 +0200

    Update function to get name of users

--- src/Functions/GetName.js
+++ src/Functions/GetName.js
@@ -1,53 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import firebase, {auth} from './Firebase.js';
+import firebase                       from './Firebase';
 
-//--------------------------------------------------------------/
-//
-//
-// This functions returns the name of a user 
-// with a specific userUid
-//
-//
-//--------------------------------------------------------------/
-const GetName = (userUid) => {
+const GetName = (uid) => {
     
-    const [name, setName]   = useState(null);
+    const [userName, setUserName] = useState(null);
 
-    // Getting users posts
     useEffect( () => { 
-        firebase.database().ref('posts/').on('value', snapshot => { 
-
-                    // Capturing data
-                    var posts = snapshot.val(); 
-
-                    // We sum spicy points of all the posts written by user with ID = uid
-                    if(posts){
-
-                        Object.keys(posts).map( id => { 
-
-                            // Counting the posts is easy, only increment value by one
-                            if(posts[id].userUid === userUid) setName(posts[id].userName);
-                            
-                            // Getting all the replies
-                            if(typeof posts[id].replies !== 'undefined'){
-
-                                var replies = posts[id].replies;
-
-                                // Count same way as we did before, increment value by one                      
-                                Object.keys(replies).map( id => {
-
-                                    if(replies[id].userUid === userUid) setName(replies[id].userName);
-                                });
-                            }
-                            
-                        });
-                    }
+        
+        if(uid){
+            
+            firebase.database().ref(`users/${uid}/name`).once('value').then(snapshot => {
+               
+                let name = snapshot.val();
                 
-        });
+                setUserName(name);
+                
+            });
+            
+        }
         
-    }, [userUid]);
-        
-    return name;
+    }, [uid]);
+    
+    return userName;
+    
 }
 
 export default GetName;
