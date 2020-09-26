b28f0da0767bc537ba33b86d5d543db0e47d81b1
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-09-26 10:54:45 +0200

    Deleted comments

--- src/Functions/NormalizeWord.js
+++ src/Functions/NormalizeWord.js
@@ -1,19 +1,14 @@
 const normalize = (word) => {
     
-    // Removing spaces and adding hyphen
     let hyphenWord = word.split(' ').join('-');
     
-    // Defining accents and normal chars
     let accents = 'ÀÁÂÃÄÅĄĀāàáâãäåąßÒÓÔÕÕÖØŐòóôőõöøĎďDŽdžÈÉÊËĘèéêëęðÇçČčĆćÐÌÍÎÏĪìíîïīÙÚÛÜŰùűúûüĽĹŁľĺłÑŇŃňńŔŕŠŚŞšśşŤťŸÝÿýŽŻŹžżźđĢĞģğ';
     let normal  = 'AAAAAAAAaaaaaaaasOOOOOOOOoooooooDdDZdzEEEEEeeeeeeCcCcCcDIIIIIiiiiiUUUUUuuuuuLLLlllNNNnnRrSSSsssTtYYyyZZZzzzdGGgg';
     
-    // Defining entries as a pair of key, value => [[À, A], [Á, A] ... [ğ, g]]
     let entries = [...accents].map((c,i) => [accents[i], normal[i]]);
     
-    // Building the object
     let accentsNormalObject = Object.fromEntries(entries);
     
-    // Transforming the word
     let result = [...hyphenWord].map(c => accentsNormalObject[c] || c).join('');
     
     return result.toLocaleLowerCase();
