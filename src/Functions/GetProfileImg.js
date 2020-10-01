4b088a50048aa043c5421fffd241706bbca18f3b
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-10-01 17:41:45 +0200

    Getting image profile from user's profile instead of reading all the content of the blog

--- src/Functions/GetProfileImg.js
+++ src/Functions/GetProfileImg.js
@@ -1,51 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import firebase, {auth} from './Firebase.js';
+import firebase                       from './Firebase';
 
-//--------------------------------------------------------------/
-//
-//
-// This functions returns the name of a user 
-// with a specific userUid
-//
-//
-//--------------------------------------------------------------/
-const GetProfileImg = (userUid) => {
+const GetProfileImg = (uid) => {
     
-    const [imgUrl, setImgUrl]   = useState(null);
-
-    // Getting users posts
+    const [imgUrl, setImgUrl] = useState(null);
+    
     useEffect( () => { 
-        firebase.database().ref('posts/').on('value', snapshot => { 
-
-                    // Capturing data
-                    var posts = snapshot.val(); 
-
-                    // We sum spicy points of all the posts written by user with ID = uid
-                    if(posts){
-
-                        Object.keys(posts).map( id => { 
-
-                            // Counting the posts is easy, only increment value by one
-                            if(posts[id].userUid === userUid) setImgUrl(posts[id].userPhoto);
-                            
-                            // Getting all the replies
-                            if(typeof posts[id].replies !== 'undefined'){
-
-                                var replies = posts[id].replies;
-
-                                // Count same way as we did before, increment value by one                      
-                                Object.keys(replies).map( id => {
-
-                                    if(replies[id].userUid === userUid) setImgUrl(replies[id].userPhoto);
-                                });
-                            }
-                            
-                        });
-                    }
-        });
         
-    }, [userUid]);
+        if(uid){
+            
+            firebase.database().ref(`users/${uid}/profilePic`).once('value').then(snapshot => {
+               
+                let name = snapshot.val();
+                
+                setImgUrl(name);
+                
+            });
+            
+        }
         
+    }, [uid]);
+    
     return imgUrl;
 }
 
