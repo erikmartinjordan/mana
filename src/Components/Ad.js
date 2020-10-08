9f45410a227bcab5f93963e92efe575f4baa8281
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-10-08 13:37:03 +0200

    Hidding ads because they are ugly as fuck

--- src/Components/Ad.js
+++ src/Components/Ad.js
@@ -24,7 +24,7 @@ const Ad = () => {
             }
             else{
                 
-                setDisplayAds(true);
+                setDisplayAds(false);
                 
             }
             
