27e46274422353154b5cd0f42fd949824c2ba417
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-12-15 17:02:25 +0100

    Fix line breaks

--- src/Tests/Global.test.js
+++ src/Tests/Global.test.js
@@ -3,7 +3,6 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import { act }                            from 'react-dom/test-utils';
 import { environment }                    from '../Functions/Firebase';
 
-
 it("Deployment in PRO", () => {
     
     expect(environment).toBe('PRO');
