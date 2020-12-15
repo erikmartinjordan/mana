42e6e49d1885b5ee573cda7a733dd7db2bbfdb59
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-12-15 17:02:17 +0100

    Fix indent

--- src/Components/DonateFail.js
+++ src/Components/DonateFail.js
@@ -13,11 +13,11 @@ const DonateFail = () => {
     }, [])
     
     return (
-      <div className = 'Default'>
-        <h2>Ups...</h2>
-        <img src = 'https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif' alt = {'Donate Failure'}></img>
-        <p>Algo ha ido mal en el proceso de donación... Vuelve a intentarlo más tarde o escribe un mensaje en la comunidad y miramos qué ha pasado.</p>
-      </div>
+        <div className = 'Default'>
+            <h2>Ups...</h2>
+            <img src = 'https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif' alt = {'Donate Failure'}></img>
+            <p>Algo ha ido mal en el proceso de donación... Vuelve a intentarlo más tarde o escribe un mensaje en la comunidad y miramos qué ha pasado.</p>
+        </div>
     );
 }
 
