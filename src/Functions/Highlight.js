c4980256770b153c128045a158901d5ad38ea73a
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2021-07-14 16:07:40 +0200

    Temporary fix to display links

--- src/Functions/Highlight.js
+++ src/Functions/Highlight.js
@@ -9,25 +9,35 @@ export const highlightMentions = (text) => {
 
     }
 
-    let res = [];
+    let res;
 
-    let arr = text.split(' ');
+    if(typeof text === 'string'){
 
-    arr.forEach((el, i) => {
-        
-        if(el.startsWith('@'))
-            res = [...res, <span key = {i} style = {format}>{el}</span>, ' '];
-        
-        else if(res.length === 0)
-            res[0] = el + ' ';
+        res = [];
 
-        else if(i !== arr.length - 1)
-            res[res.length - 1] += el + ' ';
+        let arr = text.split(' ');
+
+        arr.forEach((el, i) => {
+            
+            if(el.startsWith('@'))
+                res = [...res, <span key = {i} style = {format}>{el}</span>, ' '];
+            
+            else if(res.length === 0)
+                res[0] = el + ' ';
+
+            else if(i !== arr.length - 1)
+                res[res.length - 1] += el + ' ';
+            
+            else
+                res[res.length - 1] += el;
         
-        else
-            res[res.length - 1] += el;
-    
-    })
+        })
+    }
+    else{
+
+        res = text;
+
+    }
 
     return res;
 
