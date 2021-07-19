4c3355535b8f1eb64ae6e025033bc53c28094da1
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2021-07-19 09:36:01 +0200

    Fixed count number of impressions

--- src/Functions/UpdateNumberOfViews.js
+++ src/Functions/UpdateNumberOfViews.js
@@ -2,26 +2,29 @@ import firebase from './Firebase';
 
 const updateNumberOfViews = (post, url) => {
 
-    firebase.database().ref(`posts/${url}/views`).transaction(value => value + 1);
+    let unique = {};
+
+    unique[post.userUid] = true;
 
     firebase.database().ref(`users/${post.userUid}/numViews`).transaction(value => value + 1);
     
     if(post.replies){
 
-        let unique = {};
-
         Object.values(post.replies).forEach(reply => {
             
             if(!unique[reply.userUid]){
 
-                firebase.database().ref(`users/${reply.userUid}/numViews`).transaction(value => value + 1);
                 unique[reply.userUid] = true;
 
+                firebase.database().ref(`users/${reply.userUid}/numViews`).transaction(value => value + 1);
+                
             }
         
         });
     
     }
+
+    firebase.database().ref(`posts/${url}/views`).transaction(value => value + 1);
 
 }
 
