cdb240e42a44a72d585417accd84085b610a027e
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2022-02-24 10:59:28 +0100

    Removes semicolons

--- src/Components/DonateFail.js
+++ src/Components/DonateFail.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
-import '../Styles/Default.css';
+import React, { useEffect } from 'react'
+import '../Styles/Default.css'
 
 const DonateFail = () => {
     
     useEffect(() => {
         
-        let meta = document.createElement('meta'); 
-        meta.name = 'robots'; 
-        meta.content = 'noindex, nofollow';
-        document.querySelector('meta[name="description"]').after(meta);
+        let meta = document.createElement('meta') 
+        meta.name = 'robots'
+        meta.content = 'noindex, nofollow'
+        document.querySelector('meta[name="description"]').after(meta)
         
     }, [])
     
@@ -18,7 +18,7 @@ const DonateFail = () => {
             <img src = 'https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif' alt = {'Donate Failure'}></img>
             <p>Algo ha ido mal en el proceso de donación... Vuelve a intentarlo más tarde o escribe un mensaje en la comunidad y miramos qué ha pasado.</p>
         </div>
-    );
+    )
 }
 
-export default DonateFail;
+export default DonateFail
