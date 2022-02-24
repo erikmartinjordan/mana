2219c6d6fca70a1a822c69bb5654cfd789778748
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2022-02-24 10:12:27 +0100

    Deletes useless lines

--- src/Components/Tags.js
+++ src/Components/Tags.js
@@ -1,30 +1,17 @@
 import React, { useEffect, useState }                        from 'react'
 import { Link }                                              from 'react-router-dom'
-import Twemoji                                               from './Twemoji'
 import { db, limitToLast, onValue, query, ref, orderByChild} from '../Functions/Firebase'
-import { ReactComponent as ThaiFlag }                        from '../Assets/thailand.svg'
 import '../Styles/Tags.css'
 
-const Tags = ({numberOfTags = 10}) => {
+const Tags = ({ numberOfTags = 10 }) => {
     
     const [tags, setTags] = useState([])
     
     useEffect(() => {
         
         let unsubscribe = onValue(query(ref(db, 'tags'), orderByChild('counter'), limitToLast(numberOfTags)), snapshot => {
-            
-            if(snapshot.val()){
-                
-                let sorted = Object.entries(snapshot.val()).sort((a, b) => b[1].counter  - a[1].counter)
-                
-                setTags(sorted)
-                
-            }
-            else{
-                
-                setTags([])
-                
-            }
+
+            setTags(Object.entries(snapshot.val() || {}).sort((a, b) => b[1].counter  - a[1].counter))
             
         })
         
